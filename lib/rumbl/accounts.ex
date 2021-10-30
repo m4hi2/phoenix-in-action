cdda9fb8a7153f6febb1e105f6b08f6ec27a60a7
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-10-30 12:17:38 +0600

    refactor: used pipes for input to enum

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -14,11 +14,13 @@ defmodule Rumbl.Accounts do
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    list_users()
+    |> Enum.find(fn map -> map.id == id end)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
+    list_users()
+    |> Enum.find(fn map ->
       Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end )
     end)
   end
