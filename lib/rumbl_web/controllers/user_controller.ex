45d85c77de5d5947e4639638c170b20bc192ce01
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-11-07 06:45:46 +0600

    feat: authenticating using plug

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -4,14 +4,11 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
+  plug :authenticate when action in [:index, :show]
+
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-      conn ->
-        users = Accounts.list_users()
-        render(conn, "index.html", users: users)
-    end
+    users = Accounts.list_users()
+    render(conn, "index.html", users: users)
   end
 
   def show(conn, %{"id" => id}) do
@@ -36,7 +33,7 @@ defmodule RumblWeb.UserController do
     end
   end
 
-  defp authenticate(conn) do
+  defp authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
