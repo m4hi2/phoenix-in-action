afddc84df8dd5b9f97c88b4dda56892587e65d94
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-04 09:41:15 +0600

    feat: prepeared for reading diving into ecto queries chapter

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -8,6 +8,7 @@ defmodule Rumbl.Multimedia do
 
   alias Rumbl.Accounts
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.Category
 
   @doc """
   Returns the list of videos.
@@ -118,5 +119,15 @@ defmodule Rumbl.Multimedia do
   """
   def change_video(%Video{} = video, attrs \\ %{}) do
     Video.changeset(video, attrs)
+  end
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
   end
 end
