0a6f7ae283f51afd6f76f8c0466484e11bc7b941
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-09 03:38:40 +0600

    feat: slug field to video module

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -6,6 +6,7 @@ defmodule Rumbl.Multimedia.Video do
     field :description, :string
     field :title, :string
     field :url, :string
+    field :slug, :string
     belongs_to :user, Rumbl.Accounts.User
     belongs_to :category, Rumbl.Multimedia.Category
 
