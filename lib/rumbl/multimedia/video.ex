935f21fd2337aa299d8bbb2af5ab98282a17b089
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-08 22:03:13 +0600

    feat: video slugify

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -18,5 +18,20 @@ defmodule Rumbl.Multimedia.Video do
     |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
     |> assoc_constraint(:category)
+    |> slugify_title()
+  end
+
+  defp slugify_title(changeset) do
+    case fetch_change(changeset, :title) do
+      {:ok, new_title} ->
+        put_change(changeset, :slug, slugify(new_title))
+      :error -> changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^|w-]+/u, "-")
   end
 end
